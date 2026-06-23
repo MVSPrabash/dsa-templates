2287cee576d1b8f7c7150b8f8fd2814eb8795169
Prabash <mvsprabash@gmail.com>
2026-06-23 21:44:34 +0530

    feat: add CF C++ template

---
+++ cf_template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using i64 = int64_t;
+using u64 = uint64_t;
+using u32 = uint32_t;
+using i128 = __int128;
+using u128 = unsigned __int128;
+
+void solve() {
+    
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+
+    while(t--) {
+        solve();
+    }
+}
